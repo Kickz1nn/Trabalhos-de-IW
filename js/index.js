210f60db7e6729a35ae2cc1ba3ca8fd63bee6dbc
Caio <oliveiradacruzcaio@gmail.com>
2024-06-10 22:09:08 -0300

    Condição de erro

--- js/index.js
+++ js/index.js
@@ -6,4 +6,7 @@ function Calcular() {
 
     altxt = document.getElementById("ptotal").innerHTML = "Total Geral: " + nome + ", você tem " + total + " horas de aula nessa semana.";
     
+    if( nome == "" || horas == "" || aulas == "") {
+        altxt = document.getElementById("ptotal").innerHTML = "Algum valor não foi inserido, tente novamente!"
+    }
 }
